156b0a216d2f4036da644d0c8fdb4aaf00f80b3d
Leandro Guida <146452901+lguida-nasdaq@users.noreply.github.com>
2024-07-22 14:21:00 -0400

    Update server.go

--- server.go
+++ server.go
@@ -16,6 +16,9 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+    for _, e := range os.Environ() {
+        fmt.Println(e)
+    }
     http.HandleFunc("/", indexHandler)
     log.Printf("Starting HTTP server at port: %d\n", port)
     log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
