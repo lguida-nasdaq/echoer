5c6d98e913c1090c2d9eecc0fd2f999a49cf423b
Leandro Guida <leandro.guida@nasdaq.com>
2025-05-16 14:10:23 -0400

    Test

--- server.go
+++ server.go
@@ -1,3 +1,4 @@
+// Some comment
 package main
 
 import (
@@ -10,16 +11,16 @@ import (
 const port = 8080
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-    for _, envvar := range os.Environ() {
-        w.Write([]byte(fmt.Sprintf("%s\n", envvar)))
-    }
+	for _, envvar := range os.Environ() {
+		w.Write([]byte(fmt.Sprintf("%s\n", envvar)))
+	}
 }
 
 func main() {
-    for _, e := range os.Environ() {
-        fmt.Println(e)
-    }
-    http.HandleFunc("/", indexHandler)
-    log.Printf("Starting HTTP server at port: %d\n", port)
-    log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
+	for _, e := range os.Environ() {
+		fmt.Println(e)
+	}
+	http.HandleFunc("/", indexHandler)
+	log.Printf("Starting HTTP server at port: %d\n", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
 }
